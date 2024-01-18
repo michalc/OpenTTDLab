e67a4a6cd62d11f2207a7d2a846687bc08177ed5
Michal Charemza <michal@charemza.name>
2024-01-18 05:38:10 +0000

    feat: initial run_experiment and get_config functions
    This sets up empty skeleton functions to run the experiment and to get config

--- test_openttdlab.py
+++ test_openttdlab.py
@@ -2,7 +2,10 @@ from openttdlab import setup_experiment, save_config, load_config
 
 
 def test_setup_experiment():
-	setup_experiment()
+	run_experiment, get_config = setup_experiment()
+
+	run_experiment()
+	get_config()
 
 
 def test_save_config():
