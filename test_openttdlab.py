d82594c117e6c65cb291e59b702bcda4abea82d2
Michal Charemza <michal@charemza.name>
2024-01-20 16:14:44 +0000

    feat: extract (very unusable) results
    A very small step towards actually extracting something of value - returning
    the binary contents of a single savegame file

--- test_openttdlab.py
+++ test_openttdlab.py
@@ -3,9 +3,10 @@ from openttdlab import setup_experiment, save_config, load_config
 
 def test_setup_experiment():
 	run_experiment, get_config = setup_experiment()
-
-	run_experiment()
 	get_config()
+
+	results = run_experiment()
+	assert results
 
 
 def test_save_config():
