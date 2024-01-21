cd3e131003219380ac825f89b60ba26ff220e3a6
Michal Charemza <michal@charemza.name>
2024-01-21 15:39:34 +0000

    feat: run for a range of seeds

--- test_openttdlab.py
+++ test_openttdlab.py
@@ -4,13 +4,14 @@ from openttdlab import run_experiment, save_config, load_config
 def test_run_experiment():
     results, config = run_experiment(
         days=365 * 5 + 1,
+        seeds=range(2, 4),
         ais=(
             ('trAIns', './fixtures/54524149-trAIns-2.1.tar'),
         ),
     )
 
-    assert len(results) == 59
-    assert results[0]['PLYR']['0']['name'] == 'trAIns AI'
+    assert len(results) == 118
+    assert results[0][1]['PLYR']['0']['name'] == 'trAIns AI'
 
 
 def test_save_config():
